b5bfa9dad874610a8521460c485f006c568263ff
MarkSian <merksianipar@gmail.com>
2025-02-20 18:59:34 -0700

    Frontend Skeleton
    <NavBar />
    <ContentList/>
    <MoodFilter />
    <Footer />

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import NavBar from './components/NavBar'
+import MoodFilter from './components/MoodFilter'
+import ContentList from './components/ContentList'  
+import Footer from './components/Footer' 
+
+function App() {
+
+
+  return (
+    <>
+      <NavBar />
+      <ContentList/>
+      <MoodFilter />
+      <Footer />
+      
+      
+    </>
+  )
+}
+
+export default App
