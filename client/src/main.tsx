af460c9c2efc61102974c508b32b406f222ae1eb
MarkSian <merksianipar@gmail.com>
2025-02-28 14:27:03 -0700

    Render Changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App.tsx';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql', // Replace with your GraphQL endpoint
+  uri: 'https://anivault.onrender.com/graphql', // Replace with your GraphQL endpoint
 });
 
 const authLink = setContext((_, { headers }) => {
