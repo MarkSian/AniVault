966d241df66c56c43fb5a0ed27533c09554048e4
MarkSian <merksianipar@gmail.com>
2025-03-02 16:47:25 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
 import client from './graphql/client'; // Import the Apollo Client setup
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
