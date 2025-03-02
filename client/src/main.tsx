3cbaf578fd64443331d2046f366de6724bd20bd0
MarkSian <merksianipar@gmail.com>
2025-03-02 16:45:07 -0700

    render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphql/client'; // Import the Apollo Client setup
+import App from './App';
 import './index.css';
-import App from './App.tsx';
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql', // Replace with your GraphQL endpoint
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
