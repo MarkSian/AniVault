e2ee0dc675a1c931124655b7d39b3a3279a0d0c4
MarkSian <merksianipar@gmail.com>
2025-03-02 17:21:25 -0700

    Update client.ts

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://anivault.onerender.com/graphql', // Use environment variable for endpoint
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'https://anivault.onrender.com/graphql', // Corrected endpoint URL
 });
 
 const authLink = setContext((_, { headers }) => {
